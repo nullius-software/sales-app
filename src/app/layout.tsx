03b7269555b3287aaee1fd362a855f9868fb071b
KevOrtega <kevortega.dev@gmail.com>
2025-06-08 08:29:33 -0300

    feat: change notifications colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-        <Toaster />
+        <Toaster richColors />
         <AuthChecker />
       </body>
     </html>
