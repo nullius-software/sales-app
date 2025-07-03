1309f844686b1d02f57656c6f33f167dbefcbd06
KevOrtega <kevortega.dev@gmail.com>
2025-07-03 02:57:43 -0300

    feat: improve auth responsiveness

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,9 +5,10 @@ import { Suspense } from "react";
 
 export default async function LoginPage() {
     return (
-        <div className="h-screen flex justify-center items-center sm:bg-gray-100">
-            <div className="max-w-md w-full px-4 py-8 sm:bg-white rounded sm:shadow-md">
-                <h1 className="text-2xl font-bold mb-4">Inicia Sesión en Nullius</h1>
+        <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+            <h1 className="my-auto text-2xl font-bold mb-4 sm:hidden">Inicia Sesión en Nullius</h1>
+            <div className="mt-auto w-full px-4 py-8 bg-white rounded-t-4xl shadow-md min-h-1/2 sm:min-h-0 sm:rounded sm:w-md sm:mt-0">
+                <h2 className="text-2xl font-bold mb-4 hidden sm:block">Inicia Sesión en Nullius</h2>
                 <Suspense fallback={<div>Loading...</div>}>
                     <LoginForm />
                 </Suspense>
