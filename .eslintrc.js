32b1c676938aa988ddec1ee37413339729f77053
Giuliano Contino <f15ur4.97@gmail.com>
2025-07-07 19:59:54 -0300

    feat: añadida configuracion de soporte para eslint con typescript en nextjs

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "next/core-web-vitals",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: ["./tsconfig.eslint.json"],
+        tsconfigRootDir: __dirname,
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+
+        //declaring 'next/core-web-vitals' and 'prettier' again in case
+        //the two plugin:... configs above overrode any of their rules
+        //Also, 'prettier' needs to be last in any extends array
+        "next/core-web-vitals",
+        "prettier",
+      ],
+    },
+  ],
+};
