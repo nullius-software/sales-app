9818bcd41a82c40b853c1055e22adc0cc60cbd7c
KevOrtega <kevortega.dev@gmail.com>
2025-07-08 13:17:53 -0300

    fix: email glitch at large email

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -13,7 +13,7 @@ export default async function Navigation() {
         <div className="flex flex-col h-full py-6 px-2">
             <div className="px-4 mb-6">
                 <p className="text-sm text-gray-500">Sesión iniciada como</p>
-                <p className="font-medium">{email}</p>
+                <p className="font-medium w-full truncate">{email}</p>
             </div>
             <div className="flex-grow flex flex-col justify-center">
                 <div className="mb-12">
