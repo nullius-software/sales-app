4b2c9668aa8291b24d04812abcc90c299a0dc86d
KevOrtega <kevortega.dev@gmail.com>
2025-07-19 09:45:38 -0300

    feat: use middleware to update accessToken in case we have refresh before checking user

--- src/lib/auth/introspect.ts
+++ src/lib/auth/introspect.ts
@@ -1,23 +1,24 @@
-import axios from 'axios';
-import { cookies } from 'next/headers';
+"use server";
+
+import axios from "axios";
+import { cookies } from "next/headers";
 
 export async function introspectToken() {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get('access_token')?.value;
-    if (!accessToken) throw new Error('No access token found');
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("access_token")?.value;
+  if (!accessToken) throw new Error("No access token found");
 
-    const params = new URLSearchParams();
-    params.append('token', accessToken);
-    params.append('client_id', process.env.KEYCLOAK_CLIENT_ID!);
-    params.append('client_secret', process.env.KEYCLOAK_CLIENT_SECRET!);
+  const params = new URLSearchParams();
+  params.append("token", accessToken);
+  params.append("client_id", process.env.KEYCLOAK_CLIENT_ID!);
+  params.append("client_secret", process.env.KEYCLOAK_CLIENT_SECRET!);
 
-    const { data } = await axios.post(
-        `${process.env.KEYCLOAK_URL}/realms/nullius-realm/protocol/openid-connect/token/introspect`,
-        params,
-        {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        }
-    );
-
-    return data;
+  await axios.post(
+    `${process.env.KEYCLOAK_URL}/realms/nullius-realm/protocol/openid-connect/token/introspect`,
+    params,
+    {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }
+  );
+  return;
 }
