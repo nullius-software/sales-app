1135399f6f3643700738d6fb9c6e509622f1f6ee
KevOrtega <kevortega.dev@gmail.com>
2025-07-31 11:03:39 -0300

    feat: add n8n chat

---
+++ src/app/(protected)/chat/page.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Chat from "./chat";
+import { getCurrentUser } from "@/lib/auth/getCurrentUser";
+
+export default async function ChatPage() {
+  const user = await getCurrentUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex flex-col flex-1 p-4 bg-gray-50">
+      <Card className="m-4 h-full">
+        <CardHeader>
+          <CardTitle>Asistente Nullius</CardTitle>
+        </CardHeader>
+        <CardContent className="h-full">
+          <Chat userId={user.id} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
